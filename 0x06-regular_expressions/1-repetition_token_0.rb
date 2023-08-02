2aec34a28d971bf21cc0ba17ac611fc3b4d1ac8a
michaeljumah <michealjumah8@gmail.com>
2023-08-02 10:08:26 -0700

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Ruby script that accepts one argument and pass it to a regular expression matching method
+#Ruby script that accepts 1 agument,pass it t regular expression matching method
 puts ARGV[0].scan(/hb+t{2,5}n/).join
