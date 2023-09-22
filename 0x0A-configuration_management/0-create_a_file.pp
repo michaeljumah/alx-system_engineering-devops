40a5d5731eaa8e4d04b88e837f876b16fc2c71b0
michaeljumah <michaeljumah8@gmail.com>
2023-09-22 11:40:14 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create file using Puppet
+
+file { '/tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
