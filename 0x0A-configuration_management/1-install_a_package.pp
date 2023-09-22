bb86ad1b3514a143d69588952a691acae23a1475
michaeljumah <michaeljumah8@gmail.com>
2023-09-22 12:32:10 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Install flask from pip3 using Puppet
 
-exec { 'puppet-lint':
-  command => '/bin/gem install puppet-lint -v 2.5.0',
+package { "flask":
+	ensure => '2.1.0'
+	provider => 'pip3'
 }
