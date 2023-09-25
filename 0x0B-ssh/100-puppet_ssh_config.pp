ce2ba605f83c838fee2faa1c2ee57016f47097a4
michaeljumah <michaeljumah8@gmail.com>
2023-09-25 05:33:23 -0700

    *

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Configure the SSH server to authenticates only through SSh keys
+
+file_line { ' Turn off passwd auth':
+  path     => '/etc/ssh/ssh_config',
+  line     => ' PasswordAuthentication no',
+  replace  => true,
+}
+file_line { 'Declare identity file':
+  path     => '/etc/ssh/ssh_config',
+  line     => ' IdentityFile ~/.ssh/school',
+  replace  => true,
+}
